9030add5df0254405f312b4d9257145cab516339
Deepak Kumar <kumardeep4591@gmail.com>
2024-04-24 00:33:21 +0530

    Thymeleaf-template concept implemented

--- Thymeleaf-3/src/main/java/com/spring/controllers/HomeController.java
+++ Thymeleaf-3/src/main/java/com/spring/controllers/HomeController.java
@@ -1,13 +1,27 @@
 package com.spring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class HomeController {
 
 	@GetMapping({"/","/home"})
-	public String home() {
+	public String home(Model model) {
+		model.addAttribute("title", "MySunShine");
+		model.addAttribute("services", "Our Services");
 		return "home";
 	}
+	
+	@GetMapping("/aboutus")
+	public String about() {
+		return "about";
+	}
+	@GetMapping("/feedback")
+	public String feedback() {
+		return "feedback";
+	}
+	
 }
