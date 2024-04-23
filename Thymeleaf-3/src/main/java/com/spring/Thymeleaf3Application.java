e95c38ac27a447429c9bd7f1ffb5706eff3097ea
Deepak Kumar <kumardeep4591@gmail.com>
2024-04-23 22:29:08 +0530

    First commit

---
+++ Thymeleaf-3/src/main/java/com/spring/Thymeleaf3Application.java
@@ -0,0 +1,14 @@
+package com.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Thymeleaf3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Thymeleaf3Application.class, args);
+		System.err.println("\n\t!!..Application has been started..!!");
+	}
+
+}
