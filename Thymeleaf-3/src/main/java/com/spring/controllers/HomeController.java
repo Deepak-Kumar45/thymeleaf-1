f754a8db470b55520e3fb8839f2cc14a5960051e
gaugedata <gaugedata@gaugedata-OptiPlex-3020>
2024-04-24 13:23:07 +0530

    Login-form validation complete

--- Thymeleaf-3/src/main/java/com/spring/controllers/HomeController.java
+++ Thymeleaf-3/src/main/java/com/spring/controllers/HomeController.java
@@ -1,56 +1,71 @@
 package com.spring.controllers;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.spring.entities.LoginDto;
 
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
-
-
 
 @Controller
 public class HomeController {
 
-	@GetMapping({"/","/home"})
+	@GetMapping({ "/", "/home" })
 	public String home(Model model) {
 		model.addAttribute("title", "MySunShine");
 		model.addAttribute("services", "Our Services");
 		return "home";
 	}
-	
+
 	@GetMapping("/aboutus")
 	public String about() {
 		return "about";
 	}
+
 	@GetMapping("/feedback")
 	public String feedback() {
 		return "feedback";
 	}
-	
+
 	@GetMapping("/login-form")
 	public String showLoginForm(Model model) {
-		model.addAttribute("loginDto", new LoginDto());
+		LoginDto dto = new LoginDto();
+
+		model.addAttribute("loginDto", dto);
 		return "login-form";
 	}
-	
+
 	@PostMapping("/process-login")
-	public String processLogin(@Valid @ModelAttribute("loginDto") LoginDto loginDto,BindingResult result)  {
-		if(result.hasErrors()) {
+	public String processLogin(@Valid @ModelAttribute("loginDto") LoginDto dto, BindingResult result,HttpSession session) {
+		System.out.println("\nLogin-form Info:" + dto + "\n");
+		if (result.hasErrors()) {
+			List<ObjectError> allErrors = result.getAllErrors();
+			for (ObjectError objectError : allErrors) {
+				System.err.println(objectError);
+			}
 			return "login-form";
-		}else {
+		} else {
+			session.setAttribute("loginDto", dto);
 			return "redirect:/success";
 		}
+
 	}
-	
+
 	@GetMapping("/success")
-	public String success() {
+	public String success(HttpSession session,Model model) {
+		Object dto = (LoginDto)session.getAttribute("loginDto");
+		model.addAttribute("loginDto",dto);
 		return "success";
 	}
-	
+
 }
--- Thymeleaf-3/src/main/java/com/spring/entities/LoginDto.java
+++ Thymeleaf-3/src/main/java/com/spring/entities/LoginDto.java
@@ -3,6 +3,7 @@ package com.spring.entities;
 import org.springframework.stereotype.Component;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,9 +16,46 @@ import lombok.NoArgsConstructor;
 public class LoginDto {
 	
 	@NotBlank(message = "usermail cannot be empty")
+	@Pattern(regexp = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$", message = "Invalid Mail-ID")
 	private String userMail;
+
 
 	@NotBlank(message = "username cannot be empty")
 	@Size(min = 3,max = 12,message = "user name should be 3-12 character long")
 	private String userName;
+	
+	public String getUserMail() {
+		return userMail;
+	}
+
+	public void setUserMail(String userMail) {
+		this.userMail = userMail;
+	}
+
+	@Override
+	public String toString() {
+		return "LoginDto [userMail=" + userMail + ", userName=" + userName + "]";
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public LoginDto(
+			@NotBlank(message = "usermail cannot be empty") @Pattern(regexp = "/([a-zA-Z0-9]+)([\\.{1}])?([a-zA-Z0-9]+)\\@gmail([\\.])com/g", message = "Invalid Mail-ID") String userMail,
+			@NotBlank(message = "username cannot be empty") @Size(min = 3, max = 12, message = "user name should be 3-12 character long") String userName) {
+		super();
+		this.userMail = userMail;
+		this.userName = userName;
+	}
+
+	public LoginDto() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
 }
